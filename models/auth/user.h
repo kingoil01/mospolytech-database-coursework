df8d75bfb833cdaa91a724452c3635131c022beb
Sergey Grishin <sergey250307@gmail.com>
2026-06-18 17:56:51 +0300

    authmodel перемещён в models/auth

---
+++ models/auth/user.h
@@ -0,0 +1,24 @@
+#ifndef USER_H
+#define USER_H
+
+#include <QString>
+
+class User {
+public:
+    User() : m_idUser(0), m_hasCustomerProfile(false) {}
+    User(int id, const QString &login, const QString &email, bool hasProfile)
+        : m_idUser(id), m_login(login), m_email(email), m_hasCustomerProfile(hasProfile) {}
+
+    int idUser() const { return m_idUser; }
+    QString login() const { return m_login; }
+    QString email() const { return m_email; }
+    bool hasCustomerProfile() const { return m_hasCustomerProfile; }
+
+private:
+    int m_idUser;
+    QString m_login;
+    QString m_email;
+    bool m_hasCustomerProfile; // Флаг: заполнена ли уже запись в customers
+};
+
+#endif // USER_H
